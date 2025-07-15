23f59db50f02ae5c5fc3cffc54a4a312e0a45183
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2025-07-15 16:17:39 +0200

    NEW reservation status colored

--- src/core/components/reservation-status/reservation-status.component.ts
+++ src/core/components/reservation-status/reservation-status.component.ts
@@ -5,11 +5,24 @@ import { ReservationStatus, ReservationStatusTranslations } from '@core/lib/inte
   selector: 'app-reservation-status',
   standalone: true,
   imports: [],
-  templateUrl: './reservation-status.component.html',
-  styleUrl: './reservation-status.component.scss'
+  template: `
+  @if(status) {
+    <span class="font-bold" [style]="styles[status]">
+      {{ translations[status].title }}
+    </span>
+  }
+  `,
 })
 export class ReservationStatusComponent {
   @Input({required: true}) status?: ReservationStatus | null;
 
   readonly translations: Record<ReservationStatus, { title: string }> = ReservationStatusTranslations;
+
+  readonly styles: Record<ReservationStatus, { color: string }> = {
+    active: { color: 'var(--tui-support-08)' },
+    cancelled: { color: 'var(--tui-support-10)' },
+    arrived: { color: 'var(--tui-support-04)' },
+    deleted: { color: 'black' },
+    noshow: { color: 'var(--tui-negative)' },
+  };
 }
