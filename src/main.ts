a9858d7f1aa65caa49e981aa634c59cd361ca108
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2026-04-26 11:17:09 +0200

    NEW installed microsoft Clarity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 /// <reference types="@angular/localize" />
 
+import Clarity from '@microsoft/clarity';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+Clarity.init('whnnuai8ok');
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
